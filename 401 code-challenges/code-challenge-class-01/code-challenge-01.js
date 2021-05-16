6cec3646dec51ade30d987b26c229bf051208638
Amjad Mesmar <Amjad.Mesmar@gmail.com>
2021-05-16 21:21:41 +0300

    401 code challenge 01

---
+++ 401 code-challenges/code-challenge-class-01/code-challenge-01.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let array = [1,2,3,4,5,6];
+let array2 = [10,9,8,7,6,5,4,3,2,1];
+
+// Method 1:
+function reverseArray (arr){
+  let newArr = arr.reverse();
+  console.log('Reverse array with reverse(): ',newArr);
+}
+
+
+// Method 2:
+function reverseArray2 (arr){
+  let newArr = [];
+  for (let i = arr.length-1; i>=0; i--){
+    newArr.push(arr[i]);
+  }
+  console.log('Reverse array with for loop :',newArr);
+  return newArr;
+}
+
+// reverseArray(array);
+reverseArray2(array);
+reverseArray(array2);
---
+++ 401 code-challenges/code-challenge-class-01/readme.md
@@ -0,0 +1,13 @@
+# Reverse an Array
+
+Write a function that takes an array and reverse it.
+
+## Whiteboard Process
+
+![Reverse Array](./Reverse-Array.jpeg)
+![Reverse Array](./Reverse-Array-Full.jpeg)
+
+## Approach & Efficiency
+
+- Time complexity: O(n).
+- Space complexity: O(1).
